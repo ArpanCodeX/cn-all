4bb66b9cfa3566b7c248c2d2744eda3fcd49d66f
Arpan Khan <150476574+ArpanCodeX@users.noreply.github.com>
2026-07-21 06:55:58 +0530

    all updated

--- Q6/sliding_window.py
+++ Q6/sliding_window.py
@@ -1,34 +1,18 @@
 window_size = int(input("Enter Window Size: "))
 total_frames = int(input("Enter Total Frames: "))
-lost = int(input("Enter Frame Number to Lose (-1 for no loss): "))
 
 sender = 0
 
 while sender < total_frames:
-
     print("\nSender Window:")
 
-    end = min(sender + window_size, total_frames)
-
-    for i in range(sender, end):
+    for i in range(sender, min(sender + window_size, total_frames)):
         print("Frame", i, "sent")
 
     print("\nReceiver:")
 
-    for i in range(sender, end):
-
-        if i == lost:
-            print("Frame", i, "Lost")
-            print("No ACK", i)
-
-            print("\nSender: Timeout")
-            print("Frame", i, "resent")
-            print("ACK", i, "received")
-
-            lost = -1      # Prevent losing the same frame again
-
-        else:
-            print("ACK", i, "received")
+    for i in range(sender, min(sender + window_size, total_frames)):
+        print("ACK", i, "received")
 
     sender += window_size
 
